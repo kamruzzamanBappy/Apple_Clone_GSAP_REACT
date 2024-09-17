10608f50147d28cdfe219349889bbf6a2df12e82
kamruzzamanBappy <kamruzzamanbappy13@gmail.com>
2024-09-17 11:21:30 +0600

    complete hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
 
-
-const App = ()=>{
-  return(
-     <main className="bg-black">
- <Navbar />
-< Hero />
- <Highlights />
-
-     </main>
-  )
-}
-export default App
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+    </main>
+  );
+};
+export default App;
