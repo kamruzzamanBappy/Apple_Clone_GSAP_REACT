629fb29927de4d7b7fbcf0819e6e9eee63480852
kamruzzamanBappy <kamruzzamanbappy13@gmail.com>
2024-09-26 13:32:38 +0600

    video

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
-
+import VideoCarousel from "./components/VideoCarousel";
 const App = () => {
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
+      <Highlights />
+      <VideoCarousel />
     </main>
   );
 };
